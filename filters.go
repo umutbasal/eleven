e5b3c3e25b1d7b0ebc44691eb36bc8fcea204573
umutbasal <umut.bsl@yandex.com>
2021-03-31 13:04:49 +0300

    english filter

--- filters.go
+++ filters.go
@@ -3,9 +3,17 @@ package main
 import "strings"
 
 func lowercaseFilter(tokens []string) []string {
-	r := make([]string, len(tokens))
+	r := make([]string, cap(tokens))
 	for i, token := range tokens {
 		r[i] = strings.ToLower(token)
+	}
+	return r
+}
+
+func stemEnglishFilter(tokens []string) []string {
+	r := make([]string, cap(tokens))
+	for i, token := range tokens {
+		r[i] = StemEnglish(token)
 	}
 	return r
 }
--- filters_test.go
+++ filters_test.go
@@ -14,3 +14,13 @@ func TestLowerCaseFilter(t *testing.T) {
 	}
 
 }
+
+func TestStemEnglishFilter(t *testing.T) {
+	actual := stemEnglishFilter([]string{"eating", "dogs"})
+	expected := []string{"eat", "dog"}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Actual %#v, expected %#v", actual, expected)
+	}
+
+}
