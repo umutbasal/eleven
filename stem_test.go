c143670105d9e40237999b79127c73e66f4faaa8
umutbasal <umut.bsl@yandex.com>
2021-03-31 13:04:22 +0300

    Test English func

--- stem_test.go
+++ stem_test.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
 
-func TestNewStemmer(t *testing.T) {
+func TestStemmer(t *testing.T) {
 	tests := []struct {
 		algorithm string
 		word      string
@@ -20,10 +19,29 @@ func TestNewStemmer(t *testing.T) {
 	for _, tc := range tests {
 		s, err := NewStemmer(tc.algorithm)
 		if err != nil {
-			fmt.Println("Error creating stemmer: " + err.Error())
+			t.Errorf("Error creating stemmer: " + err.Error())
 			os.Exit(1)
 		}
 		actual := s.Stem(tc.word)
+		expected := tc.stem
+
+		if actual != expected {
+			t.Errorf("expected %s, got %s", expected, actual)
+		}
+	}
+}
+
+func TestStemEnglish(t *testing.T) {
+	tests := []struct {
+		algorithm string
+		word      string
+		stem      string
+	}{
+		{"english", "accumulations", "accumul"},
+		{"english", "eating", "eat"},
+	}
+	for _, tc := range tests {
+		actual := StemEnglish(tc.word)
 		expected := tc.stem
 
 		if actual != expected {
