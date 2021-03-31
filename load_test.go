3afa0cc0cb2a6fb3d20cc2e2629421773d0e9d24
umutbasal <umut.bsl@yandex.com>
2021-03-31 10:47:44 +0300

    add xml load

---
+++ load_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"log"
+)
+
+func TestLoadDocuments(t *testing.T) {
+	doc, err := loadDocuments("example.xml")
+
+	if err != nil {
+		t.Fatalf("Failed to load")
+	}
+
+	actual := doc[0].Title
+	expected := "Klock"
+	fail := "Whoop"
+
+	log.Printf("%v", doc)
+	if (!strings.Contains(actual, expected)) {
+		t.Errorf("expected %s in title , actual %s", expected, actual)
+	}
+
+	if (strings.Contains(actual, fail)){
+		t.Errorf("did not expected %s to be in %s", fail, actual)
+	}
+
+	if (cap(doc) != 2){
+		t.Errorf("expected cap 2, but got %d", cap(doc))
+	}
+}
+
